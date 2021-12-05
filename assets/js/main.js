3dfa4fd128bb60149fc6bbf732047cb38400bae7
matt <r4nd3l8888@gmail.com>
2021-12-05 23:42:06 +0100

    footer update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,16 @@
-// Trigger
+// SideBarMenu
 const sidebar = document.querySelector("#sidebarMenu");
+const main = document.querySelector(".mainBlur");
+const footer = document.querySelector(".footerBlur");
 
 btns = document.getElementsByClassName("sidebarBtn");
 for (var i = 0; i < btns.length; i++) {
     btns[i].addEventListener("click", function () {
       sidebar.classList.toggle("-translate-x-full");
+      main.classList.toggle("blur-s");
+      footer.classList.toggle("blur-s");
     });
 }
+
+// Current year
+document.getElementById("year").innerHTML = new Date().getFullYear();
