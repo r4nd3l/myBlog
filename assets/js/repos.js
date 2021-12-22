8d7f8b79c14ad1a4a0bacc1e13ac33780926eb66
matt <r4nd3l8888@gmail.com>
2021-12-22 15:49:03 +0100

    github repos API update

---
+++ assets/js/repos.js
@@ -0,0 +1,30 @@
+// GitHub repos
+let pages = 3
+for (let i=1; i < pages; i++) {
+  fetch(`https://api.github.com/users/r4nd3l/repos?page=${i}&per_page=100`, {headers: {'Accept' : 'application/vnd.github.v3+json'}})
+  .then(response => response.json()).then(data => {
+    // console.log(data);
+    const root = document.querySelector('#repos');
+    data.forEach((item) => {
+      // console.log(`${item.name}`);
+      root.innerHTML += `
+        <div class="mb-4 bg-gray-50 rounded-lg border border-solid border-gray-100 hover:border-gray-300">
+          <a href="${item.html_url}" target="_blank">
+            <div class="p-4">
+              <h2 class="text-2xl font-medium">${item.name}</h2>
+              <div class="grid xl:flex justify-between">
+                <p class="truncate-2-line md:truncate-1-line pr-4 xl:pr-16">${item.description}</p>
+                <span class="flex flex-wrap xl:flex-nowrap items-end mt-2 xl:mt-0">
+                    <small class="flex text-gray-400 mr-4 xl:mr-0 xl:ml-4"><small>Dominant language</small><i class="fas fa-hashtag py-0.5 text-teal ml-2"></i>${item.language}</small>
+                </span>
+              </div>
+            </div>
+          </a>
+        </div>
+      `
+    })
+  }).catch(error => console.error(error));
+}
+
+// For later use - images in grid view 
+// https://raw.githubusercontent.com/r4nd3l/BouncingBallPreLoader/master/img/BouncingBallPreLoader.png
